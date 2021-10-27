77f03eb130c7ecfbed7b92ec4c9835c017443d86
alexbeams1898 <beamsa1@hawkmail.newpaltz.edu>
2021-10-27 12:58:26 -0400

    Added function that finds average of 2 or more values along with mocha tests

---
+++ average.js
@@ -0,0 +1,24 @@
+/**
+ * Calculates the average of two or more numbers
+ * @param  {...object} numbers The numbers that will be averaged
+ * @return {number} The average of the numbers
+ */
+function getAverage(...numbers) {
+
+    // Ensure that all args are converted to a number
+    numbers = numbers.map(x => Number(x));
+
+    // Throw error if there are not enough args to average
+    if (numbers.length < 2)
+        throw "At least two numbers are expected.";
+
+    // Throw error if any args are NaN
+    if (!numbers.every((x) => { return !isNaN(x) }))
+        throw "Numbers contain a value that is NaN or cannot be converted to a number.";
+
+    // Return average
+    return numbers.reduce((x, y) => x + y) / numbers.length;
+
+}
+
+module.exports = { getAverage };
